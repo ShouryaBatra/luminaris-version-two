749625d536fa5f2d53d19e971c2ce2ca9650c8f1
bublibub <shourya.batra@gmail.com>
2024-10-27 22:47:48 -0700

    half of shadcn set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import Login from "./routes/Login";
 import PlanBuilder from "./routes/PlanBuilder";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import SignUp from "./routes/SignUp";
+import ForgetPassword from "./routes/ForgetPassword";
+import VerifyEmail from "./routes/VerifyEmail";
+import UpdatePassword from "./routes/UpdatePassword";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <PlanBuilder />,
   },
-  
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/forget-password",
+    element: <ForgetPassword />,
+  },
+  {
+    path: "/verify-email",
+    element: <VerifyEmail />,
+  },
+  {
+    path: "/update-password",
+    element: <UpdatePassword />,
+  },
 ]);
 
 export default function App() {
