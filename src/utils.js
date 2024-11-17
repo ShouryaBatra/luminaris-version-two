6b1407080115397b7d3bd6ddd5a14db59130c33a
bublibub <shourya.batra@gmail.com>
2024-11-16 19:51:25 -0800

    fix api key to be in .env file

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { apiKey } from "../key.js";
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
